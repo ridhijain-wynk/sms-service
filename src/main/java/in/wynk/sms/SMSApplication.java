0957d6adabde76329960ebc07ccc3f9ca4f8b827
anand-wynks <anand.anand@wynk.in>
2021-11-30 16:56:25 +0530

    removing autoConfigurations for sms

--- src/main/java/in/wynk/sms/SMSApplication.java
+++ src/main/java/in/wynk/sms/SMSApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = "in.wynk")
+@SpringBootApplication(scanBasePackages = "in.wynk",exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
 public class SMSApplication implements ApplicationRunner {
 
 
