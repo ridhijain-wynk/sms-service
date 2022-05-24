2b68f9f917dbd9cd2d61d21e77d6d641bd5e45fd
zoviwynk <zuber1.khan@wynk.in>
2022-05-24 15:54:04 +0530

    added name

--- src/main/java/in/wynk/sms/constants/SMSBeanConstant.java
+++ src/main/java/in/wynk/sms/constants/SMSBeanConstant.java
@@ -1,6 +1,7 @@
 package in.wynk.sms.constants;
 
 public interface SMSBeanConstant {
+    String AIRTEL_VOICE_MESSAGE_SENDER = "airtelVoiceSender";
     String SMPP_SENDER_WRAPPER = "lobby_smpp";
     String SMPP_SENDER_MANAGER_BEAN = "SmppSenderManagerBean";
     String SMSC_CONNECTION_HOLDER_BEAN = "SmscConnectionHolderBean";
