776654d0d4c46aba83b419556ac13cc99f8fe46d
zoviwynk <zuber1.khan@wynk.in>
2022-05-24 15:27:46 +0530

    fixed

--- src/main/java/in/wynk/sms/core/service/IVoiceSmsService.java
+++
@@ -1,8 +0,0 @@
-package in.wynk.sms.core.service;
-
-import in.wynk.sms.dto.request.SmsRequest;
-import in.wynk.sms.dto.response.VoiceSmsResponse;
-
-public interface IVoiceSmsService {
-    VoiceSmsResponse sendVoiceSms(SmsRequest msisdn);
-}
