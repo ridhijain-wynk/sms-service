dd8fcafd8da347353eebb599977d22b1765ba9b3
Vishal Rajput <vishal.rajput@wynk.in>
2022-09-27 13:49:49 +0530

    fixed

--- src/main/java/in/wynk/sms/sender/SmsSenderUtils.java
+++ src/main/java/in/wynk/sms/sender/SmsSenderUtils.java
@@ -11,6 +11,7 @@ import in.wynk.sms.core.service.SenderConfigurationsCachingService;
 import in.wynk.sms.core.service.SendersCachingService;
 import in.wynk.sms.dto.request.CommunicationType;
 import in.wynk.sms.dto.request.SmsRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,7 @@ import java.util.*;
 import static in.wynk.sms.constants.SMSConstants.*;
 import static in.wynk.sms.constants.SmsLoggingMarkers.*;
 
+@Slf4j
 @Component
 public class SmsSenderUtils implements ISmsSenderUtils {
     protected Logger logger = LoggerFactory.getLogger(getClass().getCanonicalName());
@@ -42,7 +44,9 @@ public class SmsSenderUtils implements ISmsSenderUtils {
     @Override
     public Map<String, IMessageSender<SmsRequest>> fetchSmsSender(SmsRequest request) {
         Map<String, IMessageSender<SmsRequest>> senderMap = new HashMap<>();
-        addSender(senderMap, PRIMARY, LOBBY_MESSAGE_STRATEGY);
+        Map<String, String> tempMap = new HashMap<>();
+        tempMap.put(PRIMARY, LOBBY_MESSAGE_STRATEGY);
+        //addSender(senderMap, PRIMARY, LOBBY_MESSAGE_STRATEGY);
         try {
             Client client = clientDetailsCachingService.getClientByAlias(request.getClientAlias());
             if (Objects.isNull(client)) {
@@ -54,24 +58,29 @@ public class SmsSenderUtils implements ISmsSenderUtils {
                     Map<CommunicationType, SenderDetails> senderDetailsMap = senderConfigurations.getDetails().get(request.getPriority());
                     if(!CollectionUtils.isEmpty(senderDetailsMap) && senderDetailsMap.containsKey(request.getCommunicationType()) && senderDetailsMap.get(request.getCommunicationType()).isPrimaryPresent()){
                         final String primarySenderId = senderDetailsMap.get(request.getCommunicationType()).getPrimary();
-                        addSender(senderMap, PRIMARY, sendersCachingService.getSenderById(primarySenderId).getName());
+                        //addSender(senderMap, PRIMARY, sendersCachingService.getSenderById(primarySenderId).getName());
+                        tempMap.put(PRIMARY, sendersCachingService.getSenderById(primarySenderId).getName());
                         if(senderDetailsMap.get(request.getCommunicationType()).isSecondaryPresent()){
                             final String secondarySenderId = senderDetailsMap.get(request.getCommunicationType()).getSecondary();
-                            addSender(senderMap, SECONDARY, sendersCachingService.getSenderById(secondarySenderId).getName());
+                            //addSender(senderMap, SECONDARY, sendersCachingService.getSenderById(secondarySenderId).getName());
+                            tempMap.put(SECONDARY, sendersCachingService.getSenderById(secondarySenderId).getName());
                         }
                     }
                 }
             }
+            log.error(SMS_SEND_BEAN_ERROR, "Client level sender fetched for request for "+ request.getMessageId()+ "- " + request.getMsisdn());
             if(Objects.nonNull(request.getTemplateId())){
                 Messages message = messageCachingService.get(request.getTemplateId());
                 if (Objects.nonNull(message) && Objects.nonNull(message.getSender()) &&
-                        !senderMap.get(PRIMARY).equals(BeanLocatorFactory.getBean(
-                                sendersCachingService.getSenderById(message.getSender()).getName(),
-                                new ParameterizedTypeReference<IMessageSender<SmsRequest>>() {}))){
-                    senderMap.put(SECONDARY, senderMap.get(PRIMARY));
-                    addSender(senderMap, PRIMARY, sendersCachingService.getSenderById(message.getSender()).getName());
+                        !tempMap.get(PRIMARY).equalsIgnoreCase(sendersCachingService.getSenderById(message.getSender()).getName())){
+                    //senderMap.put(SECONDARY, senderMap.get(PRIMARY));
+                    //addSender(senderMap, PRIMARY, sendersCachingService.getSenderById(message.getSender()).getName());
+                    tempMap.put(SECONDARY, tempMap.get(PRIMARY));
+                    tempMap.put(PRIMARY, sendersCachingService.getSenderById(message.getSender()).getName());
                 }
             }
+            tempMap.forEach((k,v)-> addSender(senderMap, k, v));
+            log.error(SMS_SEND_BEAN_ERROR, "Template level sender fetched for request for "+ request.getMessageId()+ "- " + request.getMsisdn());
         } catch (Exception ex) {
             logger.error(SMS_SEND_BEAN_ERROR, "error while initializing message bean for msisdn - " + request.getMsisdn(), ex);
             throw ex;
