2d93e879b822d6171bd07f47ba2d0e2b61f0e1aa
zoviwynk <zuber1.khan@wynk.in>
2023-10-07 17:34:47 +0530

    fixed

--- src/main/java/in/wynk/sms/SMSApplication.java
+++ src/main/java/in/wynk/sms/SMSApplication.java
@@ -1,5 +1,6 @@
 package in.wynk.sms;
 
+import in.wynk.client.core.config.ClientCoreConfig;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = "in.wynk",exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages = "in.wynk",exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, ClientCoreConfig.class})
 public class SMSApplication implements ApplicationRunner {
 
 
